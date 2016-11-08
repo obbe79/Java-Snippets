4fdf1fdd4ecbfc4f3a8d46cb124b673c93263b4d
Rob <coolwop@gmail.com>
2016-11-08 17:31:35 +0100

    Created by Coding Ground Client

---
+++ New Project-20161107/MyProg.java
@@ -0,0 +1,20 @@
+//import java.lang.Math;
+
+public class MyProg{
+
+    public static void main(String []args){
+        
+        AtmData a1 = new AtmData();
+
+    
+        System.out.println("Baro:" + a1.getBaroAlt());
+        System.out.println("Baro:" + a1.getPmeas());
+        
+    }
+     
+     
+     
+     
+     
+     
+}
